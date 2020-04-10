f2607a32c04542a0ee9fe9cf19ef7e57fb4d038a
zmr <tzsimple@163.com>
2020-04-10 12:02:45 +0800

    old lenovo

--- baidu_cookie_read_autologin.py
+++ baidu_cookie_read_autologin.py
@@ -8,6 +8,8 @@ fp = open("cookie.txt", "r+", encoding='utf-8')
 r = fp.read()
 dict = eval(r)          #转换为字典形式
 for cookie in dict:  #遍历添加cookie
+    if 'expiry' in cookie:
+        del cookie['expiry']
     driver.add_cookie(cookie)
 time.sleep(5)
 driver.get("https://pan.baidu.com/")
